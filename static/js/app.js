05da4f390fecc6834d1ba1b3a524fc1e6de79433
Max Nguyen <chadwick.uxd@gmail.com>
2021-04-19 22:15:29 -0400

    app update

--- static/js/app.js
+++ static/js/app.js
@@ -2,4 +2,17 @@ var idSelect = d3.select("#selDataset");
 var demographicsTable = d3.select("#sample-metadata");
 var barChart = d3.select("#bar");
 var bubbleChart = d3.select("bubble");
+var gaugeChart = d3.select("gauge");
+
+function init() {
+    resetData();
+    d3.json("data/samples.json").then((data => {
+        data.names.forEach((name => {
+            var option = idSelect.append("option");
+            option.text(name);
+        }));
+        var initId = idSelect.property("value")
+        plotCharts(initId);
+    })); 
+}
 
