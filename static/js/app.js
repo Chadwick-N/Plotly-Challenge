525545e8194cb80e414ee279b68483988a1d723e
Max Nguyen <chadwick.uxd@gmail.com>
2021-05-13 14:38:18 -0400

    app update

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,21 @@ function resetData() {
     bubbleChart.html("");
     gaugeChart.html("");
 };
+
+// function to read JSON and plot charts
+function plotCharts(id) {
+    d3.json("data/samples.json").then((data => {
+        var individualMetadata = data.metadata.filter(participant => participant.id == id)[0];
+        var wfreq = individualMetadata.wfreq;
+        Object.entries(individualMetadata).forEach(([key, value]) => {
+            var newList = demographicsTable.append("ul");
+            newList.attr("class", "list-group list-group-flush");
+            var listItem = newList.append("li");
+            listItem.attr("class", "list-group-item p-1 demo-text bg-transparent");
+            listItem.text(`${key}: ${value}`);
+        });
+
+    }));
+};
+
+init();
